5fe3cb9317490ce705f082942be79f3a95e3ee52
LankyMoose <rausten93@gmail.com>
2026-02-01 02:06:07 +1300

    add client `VITE_APP_NAME` env var fallback

--- apps/client/src/env.ts
+++ apps/client/src/env.ts
@@ -3,7 +3,8 @@ const PORT = import.meta.env.VITE_API_PORT
   ? `:${import.meta.env.VITE_API_PORT}`
   : ""
 
-const APP_NAME = import.meta.env.VITE_APP_NAME
+const APP_NAME =
+  import.meta.env.VITE_APP_NAME ?? "Nodejs Multiplayer Game Starter"
 export const env = {
   HOST,
   PORT,
